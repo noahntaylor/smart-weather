d7242954ae1fb1fdceb52e98955c7e36efc96c5c
Noah Taylor <noahnt@live.ca>
2022-04-24 12:45:53 -0230

    Updated gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: 'e08cf0ff02f4b9a35089768da4a443b5',
-  apiUrl: 'https://api.openweathermap.org/data/2.5'
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-  production: false,
-  apiKey: 'e08cf0ff02f4b9a35089768da4a443b5',
-  apiUrl: 'https://api.openweathermap.org/data/2.5'
-};
